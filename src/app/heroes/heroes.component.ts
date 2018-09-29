c3ea5b94459f55fbe94081178a9d821bc94c5851
wangzhengWorktile <wangzheng@worktile.com>
2018-09-29 15:34:11 +0800

    new component messages

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,7 +20,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+    .subscribe(data => this.heroes = data);
   }
   ngOnInit() {
     this.getHeroes();
